8866485f0393e07b0a7082eec19cd7bde77ad211
aymanyori <aymanyori00@gmail.com>
2023-02-05 14:02:04 +0200

    'service-feature'

--- src/app/core/components/h-navbar/h-navbar.component.ts
+++ src/app/core/components/h-navbar/h-navbar.component.ts
@@ -1,47 +1,68 @@
 import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
-import { MatIconRegistry } from "@angular/material/icon";
-import { DomSanitizer } from "@angular/platform-browser";
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { distinctUntilChanged, filter, fromEvent, map, pairwise, share, tap, throttleTime } from 'rxjs';
+import {
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  map,
+  pairwise,
+  share,
+  tap,
+  throttleTime,
+} from 'rxjs';
 
 enum VisibilityState {
   Visible = 'visible',
-  Hidden = 'hidden'
+  Hidden = 'hidden',
 }
 
 enum Direction {
   Up = 'Up',
-  Down = 'Down'
+  Down = 'Down',
 }
 
 @Component({
   selector: 'app-h-navbar',
   templateUrl: './h-navbar.component.html',
-  styleUrls: ['./h-navbar.component.scss']
+  styleUrls: ['./h-navbar.component.scss'],
 })
 export class HNavbarComponent implements OnInit {
-
   constructor(
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer,
     private router: Router
   ) {
     this.matIconRegistry.addSvgIcon(
-      "signup",
-      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../../assets/icons/svg-signup.svg')
-    )
+      'signup',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../../../../assets/icons/svg-signup.svg'
+      )
+    );
   }
   //---------------------------------------------------------------------------------------------------------------------------------------------
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  public appointmentRoute() { this.router.navigate(['/appointment']) }
+  public homeRoute(): void {    
+    this.router.navigate(['/']);
+  }
   //---------------
-  public petshopRoute() { this.router.navigate(['/petshop']) }
+  public appointmentRoute(): void {
+    this.router.navigate(['/appointment']);
+  }
   //---------------
-  public signinRoute() { this.router.navigate(['/auth', 'signin']) }
+  public petshopRoute(): void {
+    this.router.navigate(['/petshop']);
+  }
   //---------------
-  public signupRoute() { this.router.navigate(['/auth', 'signup']) }
+  public signinRoute(): void {
+    this.router.navigate(['/auth', 'signin']);
+  }
+  //---------------
+  public signupRoute(): void {
+    this.router.navigate(['/auth', 'signup']);
+  }
   //---------------------------------------------------------------------------------------------------------------------------------------------
 
   public isVisible = false;
@@ -51,18 +72,20 @@ export class HNavbarComponent implements OnInit {
     map(() => window.pageYOffset),
     pairwise(),
     map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
-    distinctUntilChanged(),
-  )
+    distinctUntilChanged()
+  );
 
-  goingUp$ = this.scroll$.pipe(
-    filter(direction => direction === Direction.Up),
-    tap(t => console.log("UP")
+  goingUp$ = this.scroll$
+    .pipe(
+      filter((direction) => direction === Direction.Up),
+      tap((t) => console.log('UP'))
     )
-  ).subscribe(() => (this.isVisible = false));
+    .subscribe(() => (this.isVisible = false));
 
-  goingDown$ = this.scroll$.pipe(
-    filter(direction => direction === Direction.Down),
-    tap(t => console.log("DOWN")
-    )).subscribe(() => (this.isVisible = true));
-
+  goingDown$ = this.scroll$
+    .pipe(
+      filter((direction) => direction === Direction.Down),
+      tap((t) => console.log('DOWN'))
+    )
+    .subscribe(() => (this.isVisible = true));
 }
